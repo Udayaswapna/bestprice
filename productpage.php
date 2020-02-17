ac515228e48bb5d2a8dfa356934702415a986f68
Udayaswapna <54699295+Udayaswapna@users.noreply.github.com>
2020-02-17 16:43:45 +0530

    Add files via upload

---
+++ productpage.php
@@ -0,0 +1,449 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <title>BEST PRICE</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-touch-fullscreen" content="yes">
+  <meta name="HandheldFriendly" content="True">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+  <!-- Bootstrap v4.3.1 CSS -->
+  <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/theme.css">
+  <link rel="stylesheet" href="css/theme/themelibrary.css">
+  <!-- Slick CSS -->
+  <link rel="stylesheet" type="text/css" href="lib/slick/slick/slick.css">
+  <link rel="stylesheet" type="text/css" href="lib/slick/slick/slick-theme.css">
+  <!-- Magnific Popup core CSS file -->
+  <link rel="stylesheet" href="lib/Magnific-Popup-master/dist/magnific-popup.css">
+  <!-- Font Awesome Free 5.10.2 JS -->
+  <script defer src="lib/fontawesome-free-5.10.2-web/js/brands.js"></script>
+  <script defer src="lib/fontawesome-free-5.10.2-web/js/solid.js"></script>
+  <script defer src="lib/fontawesome-free-5.10.2-web/js/fontawesome.js"></script>
+</head>
+
+<body class="default">
+
+  <!-- Preloading -->
+  <div class="preloading">
+    <div class="wrap-preload">
+      <div class="cssload-loader"></div>
+    </div>
+  </div>
+  <!-- .Preloading -->
+
+  <div class="wrapper">
+    <!-- Sidebar left -->
+    <nav id="sidebarleft" class="sidenav">
+      <div id="dismiss">
+        <i class="fas fa-times"></i>
+      </div>
+      <div class="sidebar-header">
+        <h3>Account</h3>
+      </div>
+
+      <ul class="list-unstyled components">
+        <li>
+          <a href="homepage.html"><i class="fas fa-home"></i> Home</a>
+        </li>
+        <li>
+          <a href="add.php"><i class="fas fa-home"></i> Add product</a>
+        </li>
+        <li>
+          <a href="index11.php"><i class="fas fa-home"></i> product list</a>
+        </li>
+        <li>
+          <a href="cart1.php"><i class="fas fa-home"></i> Cart</a>
+        </li>
+      </ul>
+    </nav>
+    <!-- .Sidebar left -->
+      <!-- Sidebar right -->
+      <nav id="sidebarright" class="sidenav">
+        <div id="dismiss">
+          <i class="fas fa-times"></i>
+        </div>
+        <div class="sidebar-header">
+          <h3>Let's eat!</h3>
+        </div>
+        <div class="search-form">
+          <div class="search-wrapper ">
+            <input id="search">
+            <button class="ssubmit" type="submit" name="search_submit"><i class="fas fa-search"></i></button>
+          </div>
+          <ul class="right-menu">
+            <li class="right-menu-item">
+              <a href="#menu-dinner" data-toggle="collapse" aria-expanded="false" ><i class="fas fa-plus"></i>products <span><i class="fas fa-caret-down"></i></span></a>
+              <ul class="collapse collapsible-body" id="menu-dinner">
+                <li>
+                  <a href="#">Rice<div class="count">3</div></a>
+                </li>
+                <li>
+                  <a href="#">Dal & Pulses<div class="count">2</div></a>
+                </li>
+                <li>
+                  <a href="#">Ghee & Oil<div class="count">32</div></a>
+                </li>
+                <li>
+                  <a href="#">Atta & Flour<div class="count">32</div></a>
+                </li>
+                <li>
+                  <a href="#">Noodles & Pastha<div class="count">32</div></a>
+                </li>
+                <li>
+                  <a href="#">Dryfruits & Nuts<div class="count">32</div></a>
+                </li>
+                <li>
+                  <a href="#">households<div class="count">32</div></a>
+                </li>
+                <li>
+                  <a href="#">Dairy & Eggs<div class="count">32</div></a>
+                </li>
+              </ul>
+            </li>
+           
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Breakfast <div class="count">3</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Baking Recipes <div class="count">45</div></a></li>
+            <li class="right-menu-item">
+              <a href="#menu-quick" data-toggle="collapse" aria-expanded="false"><i class="fas fa-plus"></i>Quick & Easy Recipes <span><i class="fas fa-caret-down"></i></span></a>
+              <ul class="collapse collapsible-body" id="menu-quick">
+                <li>
+                  <a href="#">Seafood<div class="count">9</div></a>
+                </li>
+                <li>
+                  <a href="#">Chicken Recipes <div class="count">14</div></a>
+                </li>
+                <li>
+                  <a href="#">Beef <div class="count">7</div></a>
+                </li>
+              </ul>
+            </li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Chicken Recipes <div class="count">66</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Soup Recipes <div class="count">8</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Taco Recipes <div class="count">2</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Bread Recipes <div class="count">100</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Cake Recipes <div class="count">45</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Dessert <div class="count">43</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Healthy <div class="count">20</div></a></li>
+            <li class="right-menu-item"><a href="#"><i class="fas fa-plus"></i> Sandwiches <div class="count">93</div></a></li>
+          </ul>
+        </div>
+      </nav>
+      <!-- .Sidebar right-->
+      <!-- Page content  -->
+      <div id="content">
+        <!-- Header  -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-header">
+          <div class="container-fluid">
+            <button type="button" id="sidebarleftbutton" class="btn">
+            <i class="fas fa-align-left"></i>
+            </button>
+            <div class="logo">Foodkuy</div>
+            <button type="button" id="sidebarrightbutton" class="btn">
+            <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </nav>
+        <!-- .Header  -->
+        <!-- Content Wrap  -->
+
+  
+        <style>
+            #myform {
+    text-align: center;
+    padding: 5px;
+    border: 1px dotted #ccc;
+    margin: 2%;
+}
+.qty {
+    width: 40px;
+    height: 25px;
+    text-align: center;
+}
+input.qtyplus { width:25px; height:25px;}
+input.qtyminus { width:25px; height:25px;}
+
+
+</style>
+        <?php 
+
+$connect = mysqli_connect("localhost", "root", "", "product");
+
+ 
+?>
+        <div class="container">
+          <br/>
+            <br />
+            <br />
+
+          <?php
+          $id = (isset($_GET['id']) ? $_GET['id'] : '');
+				$query = "SELECT * FROM tbl_product where id='$id'";
+				$result = mysqli_query($connect, $query);
+				if(mysqli_num_rows($result) > 0)
+				{
+					while($row = mysqli_fetch_array($result))
+					{
+        ?>
+         <form method="post" action="cart1.php?action=add&id=<?php echo $row["id"]; ?>">
+            <div class="row">
+              <div class="col-md-5">
+
+
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="images/<?php echo $row["image"]; ?>" class="d-block w-100" alt="First slide">
+                                </div>
+                    <div class="carousel-item">
+                      <img src="images/<?php echo $row["image"]; ?>" class="d-block w-100" alt="Second slide">
+                                </div>
+                    <div class="carousel-item">
+                      <img src="images/<?php echo $row["image"]; ?>" class="d-block w-100" alt="Third slide">
+                                </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+             </div>
+
+              <div class="col-md-7">
+
+                <h2>  <?php echo '<span style="color:black; font-style:bold; font-size:25px;">' .$row["name"]; ?>
+                </h2>
+               
+
+                <form method="POST">
+                  <!--<div class="input-group">
+                    <h> This Saffola oil will take care of you and your family by providing the benefits of natural antioxidants, MUFA, PUFA, and Vitamins A and D</h>
+                  </div>-->
+                  <br/>
+                    <div class="input-group">
+                       ₹ <?php echo '<span style="color:black; font-style:bold; font-size:15px;">' .$row["price"]; ?>
+                    </div>
+                    <br/>
+                  
+
+                      <div>
+                        NO: <input type='button' value='-' class='qtyminus' field='quantity' />
+                        <input type='number'  name='quantity'  value='1' class='qty'id="quantity<?php echo $row["id"]; ?>"  />
+                        <input type='button' value='+' class='qtyplus' field='quantity' /><br/>
+                            
+                      
+                      </div>
+                      
+                      
+                    
+
+                      
+                      
+                      
+                      
+                      <br />
+
+                      
+                    
+                      <p>
+                      <input type="hidden" name="hidden_name" id="name<?php echo $row["id"]; ?>" value="<?php echo $row["name"]; ?>" />  
+                               <input type="hidden" name="hidden_price" id="price<?php echo $row["id"]; ?>" value="<?php echo $row["price"]; ?>" />  
+                      <input type="button" name="add_to_cart" id="<?php echo $row["id"]; ?>" style="margin-top:5px;" class="btn btn-warning form-control add_to_cart" value="Add to Cart" />  
+                       
+                      </p>
+
+
+                    </form>
+              </div>
+
+
+
+
+        
+              <br />
+              <br/>
+        </div>
+
+          <?php
+					}
+				}
+			?>
+
+        </div>
+        
+        <br>
+          
+        </br>
+	<!-- Footer  -->
+	<div class="footer">
+              <div class="footer-heading">Follow Us</div>
+              <div class="socmed">
+                <div class="socmed-line">
+                  <div class="socmed-item">
+                    <a href="#" class="si-icon"> <i class="fab fa-facebook-f"></i></a>
+                  </div>
+                </div>
+                <div class="socmed-line">
+                  <div class="socmed-item">
+                    <a href="#" class="si-icon"><i class="fab fa-instagram"></i></a>
+                  </div>
+                </div>
+                <div class="socmed-line">
+                  <div class="socmed-item">
+                    <a href="#" class="si-icon"><i class="fab fa-twitter"></i></a>
+                  </div>
+                </div>
+                <div class="socmed-line">
+                  <div class="socmed-item">
+                    <a href="#" class="si-icon"><i class="fab fa-youtube"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- .Footer  -->
+          </div>
+          <!-- .Page content  -->
+          <div class="overlay"></div>
+          <!-- Optional JavaScript -->
+          <!-- jQuery v3.4.1 -->
+          <script src="lib/jquery/jquery-3.4.1.min.js"></script>
+          <!--  Bootstrap v4.3.1 JS -->
+          <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+          <!-- Magnific Popup core JS file -->
+          <script src="lib/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
+          <!-- Slick JS -->
+          <script src="lib/slick/slick/slick.min.js"></script>
+          <!--  Custom JS -->
+          <script src="js/theme.js"></script>
+        </div>
+	</body>
+</html>
+<script>
+                        jQuery(document).ready(function () {
+                        $('.qtyplus').click(function (e) {
+
+                        e.preventDefault();
+
+                        fieldName = $(this).attr('field');
+
+                        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+
+                        if (!isNaN(currentVal)) {
+
+                        $('input[name=' + fieldName + ']').val(currentVal + 1);
+                        } else {
+
+                        $('input[name=' + fieldName + ']').val(1);
+                        }
+                        });
+
+                        $(".qtyminus").click(function (e) {
+
+                        e.preventDefault();
+
+                        fieldName = $(this).attr('field');
+
+                        var currentVal = parseInt($('input[name=' + fieldName + ']').val());
+
+                        if (!isNaN(currentVal) && currentVal > 0) {
+
+                        $('input[name=' + fieldName + ']').val(currentVal - 1);
+                        } else {
+
+                        $('input[name=' + fieldName + ']').val(0);
+                        }
+                        });
+                        });
+                      </script>
+<script>  
+ $(document).ready(function(data){  
+      $('.add_to_cart').click(function(){  
+           var product_id = $(this).attr("id");  
+           var product_name = $('#name'+product_id).val();  
+           var product_price = $('#price'+product_id).val();  
+           var product_quantity = $('#quantity'+product_id).val();  
+           var action = "add";  
+           if(product_quantity > 0)  
+           {  
+                $.ajax({  
+                     url:"action.php",  
+                     method:"POST",  
+                     dataType:"json",  
+                     data:{  
+                          product_id:product_id,   
+                          product_name:product_name,   
+                          product_price:product_price,   
+                          product_quantity:product_quantity,   
+                          action:action  
+                     },  
+                     success:function(data)  
+                     {  
+                          $('#order_table').html(data.order_table);  
+                          $('.badge').text(data.cart_item);  
+                          alert("Product has been Added into Cart");  
+                     }  
+                });  
+           }  
+           else  
+           {  
+                alert("Please Enter Number of Quantity")  
+           }  
+      });  
+      $(document).on('click', '.delete', function(){  
+           var product_id = $(this).attr("id");  
+           var action = "remove";  
+           if(confirm("Are you sure you want to remove this product?"))  
+           {  
+                $.ajax({  
+                     url:"action.php",  
+                     method:"POST",  
+                     dataType:"json",  
+                     data:{product_id:product_id, action:action},  
+                     success:function(data){  
+                          $('#order_table').html(data.order_table);  
+                          $('.badge').text(data.cart_item);  
+                     }  
+                });  
+           }  
+           else  
+           {  
+                return false;  
+           }  
+      });  
+      $(document).on('keyup', '.quantity', function(){  
+           var product_id = $(this).data("product_id");  
+           var quantity = $(this).val();  
+           var action = "quantity_change";  
+           if(quantity != '')  
+           {  
+                $.ajax({  
+                     url :"action.php",  
+                     method:"POST",  
+                     dataType:"json",  
+                     data:{product_id:product_id, quantity:quantity, action:action},  
+                     success:function(data){  
+                          $('#order_table').html(data.order_table);  
+                     }  
+                });  
+           }  
+      });  
+ });  
+ </script>
+
+
+
